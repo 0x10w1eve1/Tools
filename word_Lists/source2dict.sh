d53c03afe60c3a6b9b1f4a98841f7689ef177318
0x10w1eve1 <83690701+0x10w1eve1@users.noreply.github.com>
2022-06-20 16:16:23 -0400

    Add files via upload

--- word_Lists/source2dict.sh
+++ word_Lists/source2dict.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 if [[ -z "$1" ]];then
-	printf "\n\n\t\t\t little script with 2 functions, find and extract all archives and generate wordlist with all dirs/files\n\n\n"
-	printf "\n\n\t\t\t Issues if the filename has spaces..ends up adding it to the dict as separate entries. "
+	printf "\n\n\t\t little script with 2 functions, find and extract all archives and generate wordlist with all dirs/files\n\n\n"
+	printf "\n\n\t\t Issues if the filename has spaces..ends up adding it to the dict as separate entries. "
 
-	printf "\n\n\t\t\t [-] Usage: $0 <path with source files>\n\n"
-	printf "\n\n\t\t\t [-] Usage: $0 <path with source files> <anything here to find and extract all files in specifid dir>\n"
+	printf "\n\n\t\t [-] Usage: $0 <path with source files>\n\n"
+	printf "\n\n\t\t [-] Usage: $0 <path with source files> <anything here to find and extract all files in specifid dir>\n"
 
-	printf "\n\t\t\t [-] Wordlist is generated with name 10w1eve1_Wordlist.txt in cd \n\n"
+	printf "\n\t\t [-] Wordlist is generated with name 10w1eve1_Wordlist.txt in cd \n\n"
 	exit
 else
-	#remove trailing slash
-	sourceDir=$(echo -e $1|sed 's/\/$//')
-
+	#remove trailing slash and save sources dir name to later cut out full path for dict
+	sourcesname=$(basename $1)
+	sourceDir=$(realpath $1)
 fi
 
 
 
 FindExtract() {
 
+	printf "\n\n\t [!] Staring archive search and extraction\n"
 	#extracts all *.zip archives to subfolders with names of these archives.
 	for archive in $(find "$sourceDir" -type f -exec file \{\} +| grep "archive data"|cut -d ":" -f1);do
 		
@@ -26,13 +27,17 @@ FindExtract() {
 		printf "\n\n\t\t\t [+] Extracting $filename\n\n"
 
 		#getting output directory for unzipping
-		dirname=$(echo $archive|sed "s/$filename$//") #sed out jar filename from path
+		#dirname=$(echo $archive|sed "s/$filename$//") #sed out jar filename from path
 		# or use the actual dirname..learned of it after. lol
-		# dirname=$(dirname $archive)
+		dirname=$(dirname $archive)
 
-		unzip "$archive" -d "$dirname"
+
+		#pipe no to unzip for no overwrite. 
+		yes n|unzip "$archive" -d "$dirname"
 
 	done
+
+	printf "\n\n\t [+] Done Extracting....\n"
 
 
 }
@@ -41,6 +46,7 @@ FindExtract() {
 
 GenWordlist() {
 
+	printf "\n\n\t [+] Starting Wordlist Gen\n"
 	temp="temp10w1eve1_Wordlist.txt"
 	touch $temp
 	outfile="10w1eve1_Wordlist.txt"
@@ -48,17 +54,17 @@ GenWordlist() {
 
 
 	for dirr in $(ls -N -R "$sourceDir"|grep ":");do
-		dname=$(echo "$dirr"|sed 's/://')
-		echo $dname|tee -a $temp
-		
+		dname=$(echo -e "$dirr"|sed 's/://')
+		echo -e "${dname}/"|tee -a $temp
 		fname=$(ls -N -R -d $dname/*)
 		echo "$fname"|tr ' ' '\n' |tee -a $temp		
 
 	done
 
-	#remove empty lines and images
-	cat $temp|grep -E -v '*.img|*.svg|*.jpg|*.jpeg|*.gif|*.png'|grep . > $outfile
-	rm $temp
+	
+	#remove empty lines, images, and basepath
+	cat $temp|grep -E -v '*.img|*.svg|*.jpg|*.jpeg|*.gif|*.png'|grep . |sed "s/^.*$sourcesname//" |tee -a $outfile
+	rm "$temp"
 
 	printf "\n\n\t\t\t [!] Done....New Wordlist generated: $outfile\n\n"
 
